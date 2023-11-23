790f5369d8afccad0d0c5cc6cd93e772db49aae1
Salomon Gutierrez <salomon19982009@hotmail.com>
2023-11-23 15:19:28 -0500

    TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 export const plugins = [];
 tailwind.config = {
     theme: {
-        
         extend: {
             colors: {
                 'forest': '#06473d',
                 'sun': '#fff6a4',
                 'sunset': '#efe271',
                 'coal': '#06473d ',
-            },
-        },
+            }
+        }
     }
 }
