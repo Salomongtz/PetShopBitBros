e017bb9470c02a4528d34a108255f609d10f5126
Camila_Cein <ceincamila@gmail.com>
2023-11-25 18:53:29 -0300

    anda el filtro menos el alfabetico

--- jugueteria.js
+++ jugueteria.js
@@ -10,13 +10,13 @@ const app = createApp({
             productosOrdenados:[],
         }
     },
-    beforeCreated() {
+    beforeCreate() {
         fetch(url)
             .then(response => response.json())
             .then((data) => {
                 console.log(data)
                 this.juguetes = data.filter(item => item.categoria == "jugueteria")
-                this.productosOrdenados= this.juguetes
+                // this.productosOrdenados= this.juguetes
                 console.log(this.juguetes)
             })
             .catch(error => console.error(error))
@@ -26,16 +26,16 @@ const app = createApp({
             selec(event){
                 this.selected = event.target.value
                 console.log("Seleccionado:", this.ordenSeleccionado)
-              if(selected="menor"){
-                   this.productosOrdenados= this.productosOrdenados.slice().sort((a, b) => a.precio - b.precio)}
-                else if (selected="mayor"){
-                   this.productosOrdenados= this.productosOrdenados.slice().sort((a, b) => b.precio - a.precio)}
-                   else if(selected="alfabetico"){
+              if(this.selected=="menor"){
+                   this.juguetes= this.juguetes.slice().sort((a, b) => a.precio - b.precio)}
+                else if (this.selected=="mayor"){
+                   this.juguetes= this.juguetes.slice().sort((a, b) => b.precio - a.precio)}
+                   else if(this.selected=="alfabetico"){
              
-                   this.productosOrdenados= this.productosOrdenados.slice().sort((a, b) => a.nombre.localeCompare(b.nombre))}
+                   this.juguetes= this.juguetes.slice().sort((a, b) => a.nombre.localeCompare(b.nombre))}
                    else{
                 
-                   this.productosOrdenados= this.productosOrdenados}
+                   this.juguetes= this.juguetes}
                  
                 }
         
