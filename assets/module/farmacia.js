90ff60c266da146183e0e68021885faf964fea27
Yanina Guadalupe <yaniferreira82@gmail.com>
2023-11-27 03:04:14 -0300

    casi terminado

--- assets/module/farmacia.js
+++ assets/module/farmacia.js
@@ -3,58 +3,6 @@ const { createApp } = Vue;
 
 const opt = {
     data() {
-<<<<<<< HEAD
-      return {
-       farmacos: [],
-       filtered:[],
-       buscador:"",
-       selected:'menor',
-       verModal: false,
-       farmaco:[],
-       filtrados:[],
-    }
-},
-  created() {
-        fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-           console.log(data)
-            this.farmacos = data.filter(item => item.categoria == "farmacia");
-            console.log(this.farmacos)
-            this.filtrados= data.filter(item => item.categoria == "farmacia")
-        })
-    .catch(error => console.error(error));
-},
-methods: {
-    filtrarPorNombre(event){
-        this.buscador=event.target.value
-        this.filter()   
-    },
-    filter(){
-        const filtrados=this.farmacos.filter(farmaco=> farmaco.producto.toLowerCase().includes(this.buscador.toLowerCase()))
-        this.filtered=filtrados
-    },
-    select(event){
-        this.selected = event.target.value
-        console.log("Seleccionado:", this.ordenSeleccionado)
-        if (this.selected == "menor") {
-            this.filtrados = this.filtrados.slice().sort((a, b) => a.precio - b.precio)
-        }
-        else if (this.selected == "mayor") {
-            this.filtrados = this.filtrados.slice().sort((a, b) => b.precio - a.precio)
-        }
-        else if (this.selected == "alfabetico") {
-
-            this.filtrados = this.filtrados.slice().sort((a, b) => a.producto.localeCompare(b.producto))
-        }
-        else {
-
-            this.filtrados = this.farmacos
-        }
-
-        },
-        mostrarModal(farmaco) {
-=======
         return {
             farmacos: [],
             filtrados: [],
@@ -112,22 +60,16 @@ methods: {
             this.filtrados = filtrado
             console.log(this.filtrados)
         }, mostrarModal(farmaco) {
->>>>>>> 56761e2a758aa43355cd0dbb5be80ec9f5adb4b5
             this.farmaco = farmaco
             this.verModal = true
         },
         cerrarModal() {
             this.farmaco = {}
             this.verModal = false
-<<<<<<< HEAD
-        }
-},
-=======
         },comprar() {
             this.verCompra = true
         }
     }
->>>>>>> 56761e2a758aa43355cd0dbb5be80ec9f5adb4b5
 }
 
 const app = createApp(opt)
